3a2c78c652529d9bc78db8be1e72bae0ca747aae
Jaime Daniel <jjjsouza2015@gmail.com>
2024-07-14 14:42:53 -0300

    exemplo_aula03 modified

---
+++ exemplo_aula03.py
@@ -0,0 +1,11 @@
+# Ver exercícios para fazer no Github
+
+#x = -3
+#
+#if x <= 0:
+#    print( "é menor do que zero" )
+#elif x <= 2:
+#    print("é menor do que dois")
+#else:
+#    print('more')
+
